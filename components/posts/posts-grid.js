49b9721800d39c8284b2b16d2c330b41ed73bfff
Esteban Palacio <estipalacio@gmail.com>
2022-07-07 14:13:33 -0400

    rendered dummy data to test component structure/styling

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -6,7 +6,7 @@ const PostsGrid = (props) => {
 	return (
 		<ul className={classes.grid}>
 			{posts.map((post) => (
-				<PostItem post={post} />
+				<PostItem key={post.slug} post={post} />
 			))}
 		</ul>
 	)
