8fbc63fbaeb819cb207bbea239988c9e6564582a
Esteban Palacio <estipalacio@gmail.com>
2022-07-08 19:57:22 -0400

    added markdown content, and util.js to read through posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,15 @@ import ReactMarkdown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
 
-const DUMMY_POST = {
-	title: 'Getting Started with NextJS',
-	image: 'getting-started-nextjs.png',
-	date: '2022-02-10',
-	slug: 'getting-started-with-nextjs',
-	content: '# This is a first post',
-}
+const PostContent = (props) => {
+	const { post } = props
 
-const PostContent = () => {
-	const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+	const imagePath = `/images/posts/${post.slug}/${post.image}`
 
 	return (
 		<article className={classes.content}>
-			<PostHeader title={DUMMY_POST.title} image={imagePath} />
-			<ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+			<PostHeader title={post.title} image={imagePath} />
+			<ReactMarkdown>{post.content}</ReactMarkdown>
 		</article>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Hero from '../components/home-page/Hero'
 import FeaturedPosts from '../components/home-page/Featured-posts'
+import { getFeaturedPosts } from '../lib/posts-util'
 
 const DUMMY_POSTS = [
 	{
@@ -36,13 +37,24 @@ const DUMMY_POSTS = [
 	},
 ]
 
-const HomePage = () => {
+const HomePage = (props) => {
 	return (
 		<>
 			<Hero />
-			<FeaturedPosts posts={DUMMY_POSTS} />
+			<FeaturedPosts posts={props.posts} />
 		</>
 	)
+}
+
+export const getStaticProps = (props) => {
+	const featuredPosts = getFeaturedPosts()
+
+	return {
+		props: {
+			posts: featuredPosts,
+		},
+		revalidate: 1800,
+	}
 }
 
 export default HomePage
