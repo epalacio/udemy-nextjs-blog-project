372e74921d8d5994d86fb4eba46dc723b8212470
Esteban Palacio <estipalacio@gmail.com>
2022-07-10 16:41:32 -0400

    added Head section for relevant pages

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,16 @@
+import Head from 'next/head'
+
 import Hero from '../components/home-page/Hero'
 import FeaturedPosts from '../components/home-page/Featured-posts'
 import { getFeaturedPosts } from '../lib/posts-util'
 
-const DUMMY_POSTS = [
-	{
-		title: 'Getting Started with NextJS',
-		image: 'getting-started-nextjs.png',
-		excerpt:
-			'NextJs is the React framework for production - it makes building React fullstack apps easy to build and scale',
-		date: '2022-02-10',
-		slug: 'getting-started-with-nextjs',
-	},
-	{
-		title: 'Getting Started with NextJS2',
-		image: 'getting-started-nextjs.png',
-		excerpt:
-			'NextJs is the React framework for production - it makes building React fullstack apps easy to build and scale',
-		date: '2022-02-10',
-		slug: 'getting-started-with-nextjs2',
-	},
-	{
-		title: 'Getting Started with NextJS3',
-		image: 'getting-started-nextjs.png',
-		excerpt:
-			'NextJs is the React framework for production - it makes building React fullstack apps easy to build and scale',
-		date: '2022-02-10',
-		slug: 'getting-started-with-nextjs3',
-	},
-	{
-		title: 'Getting Started with NextJS4',
-		image: 'getting-started-nextjs.png',
-		excerpt:
-			'NextJs is the React framework for production - it makes building React fullstack apps easy to build and scale',
-		date: '2022-02-10',
-		slug: 'getting-started-with-nextjs4',
-	},
-]
-
 const HomePage = (props) => {
 	return (
 		<>
+			<Head>
+				<title>Max's Blog</title>
+				<meta name='description' content='I post about programming and web development.' />
+			</Head>
 			<Hero />
 			<FeaturedPosts posts={props.posts} />
 		</>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,18 @@
+import Head from 'next/head'
+
 import AllPosts from '../../components/posts/all-posts'
 import { getAllPosts } from '../../lib/posts-util'
 
 const AllPostsPage = (props) => {
-	return <AllPosts posts={props.posts} />
+	return (
+		<>
+			<Head>
+				<title>All Posts</title>
+				<meta name='description' content='A list of all programming posts...' />
+			</Head>
+			<AllPosts posts={props.posts} />
+		</>
+	)
 }
 
 export const getStaticProps = (props) => {
