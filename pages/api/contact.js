75d6b1a38fd47be5a147645cccf73f1f31973cf1
Esteban Palacio <estipalacio@gmail.com>
2022-07-11 10:58:06 -0400

    added next.config.js for env vars

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -31,10 +31,10 @@ const handler = async (req, res) => {
 
 		let client
 
+		const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.8broq.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
+
 		try {
-			client = await MongoClient.connect(
-				'mongodb+srv://epalacio:Rubik@cluster0.8broq.mongodb.net/my-site?retryWrites=true&w=majority'
-			)
+			client = await MongoClient.connect(connectionString)
 		} catch (error) {
 			res.status(500).json({ message: 'Could not connect to DB' })
 			return
