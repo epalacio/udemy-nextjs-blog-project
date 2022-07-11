56a1d1fb9ab9c2117e9dabe40c76f53cf3b7a807
Esteban Palacio <estipalacio@gmail.com>
2022-07-11 09:34:51 -0400

    added portal for notifs for better DOM positioning/accessibility

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,28 +1,29 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
-import classes from './notification.module.css';
+import classes from './notification.module.css'
 
 function Notification(props) {
-  const { title, message, status } = props;
+	const { title, message, status } = props
 
-  let statusClasses = '';
+	let statusClasses = ''
 
-  if (status === 'success') {
-    statusClasses = classes.success;
-  }
+	if (status === 'success') {
+		statusClasses = classes.success
+	}
 
-  if (status === 'error') {
-    statusClasses = classes.error;
-  }
+	if (status === 'error') {
+		statusClasses = classes.error
+	}
 
-  const cssClasses = `${classes.notification} ${statusClasses}`;
+	const cssClasses = `${classes.notification} ${statusClasses}`
 
-  return (
-    <div className={cssClasses}>
-      <h2>{title}</h2>
-      <p>{message}</p>
-    </div>
-  );
+	return ReactDOM.createPortal(
+		<div className={cssClasses}>
+			<h2>{title}</h2>
+			<p>{message}</p>
+		</div>,
+		document.getElementById('notifications')
+	)
 }
 
-export default Notification;
+export default Notification
